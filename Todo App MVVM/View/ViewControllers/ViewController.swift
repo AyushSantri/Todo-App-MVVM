e56d697cebd96fe2219042ad9f701246657ec35e
ayush.santri <ayushsantri222@gmail.com>
2023-04-03 15:15:41 +0530

    added table view

--- Todo App MVVM/View/ViewControllers/ViewController.swift
+++ Todo App MVVM/View/ViewControllers/ViewController.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let tableView : UITableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hey")
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        setUpTable()
     }
 
-
+    private func setUpTable() {
+        tableView.backgroundColor = .systemPink
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
