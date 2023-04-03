d036beb54d5b0155a4a49a3d5b0fc662d794f2a6
ayush.santri <ayushsantri222@gmail.com>
2023-04-03 19:12:04 +0530

    added identifier and label for cell

--- Todo App MVVM/View/CustomViews/TableViewCell.swift
+++ Todo App MVVM/View/CustomViews/TableViewCell.swift
@@ -8,16 +8,34 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    static let identifier = "TableViewCell"
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+    
+    let lable : UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 25, weight: .medium, width: .condensed)
+        label.textColor = .label
+        label.text = "Ayush Santri is Cool"
+        return label
+    }()
+}
 
+extension TableViewCell {
+    private func setUpLabel() {
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            lable.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            lable.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            lable.topAnchor.constraint(equalTo: contentView.topAnchor),
+            lable.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
 }
