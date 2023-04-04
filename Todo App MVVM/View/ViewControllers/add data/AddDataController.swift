11780a9783a9ff72184f9b060be11c9118787e6b
ayush.santri <ayushsantri222@gmail.com>
2023-04-04 22:23:48 +0530

    added text field

--- Todo App MVVM/View/ViewControllers/add data/AddDataController.swift
+++ Todo App MVVM/View/ViewControllers/add data/AddDataController.swift
@@ -11,8 +11,42 @@ class AddDataController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPink
+        setupTextField()
     }
+    
+    let textfield : TextField = {
+        let field = TextField()
+        field.placeholder = "Enter Your TODO"
+        field.font = .systemFont(ofSize: 24, weight: .medium)
+        field.borderStyle = .none
 
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.gray.cgColor
+        field.layer.cornerRadius = 5
+        field.becomeFirstResponder()
+        
+        return field
+    }()
+
+    func setupTextField() {
+        textfield.autocorrectionType = .no
+        
+        view.addSubview(textfield)
+        
+        
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textfield.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            textfield.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            textfield.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            textfield.heightAnchor.constraint(equalToConstant: 24*2.3)
+        ])
+    }
+}
+
+public class TextField : UITextField {
+    public override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 20, dy: 0)
+    }
 }
