00012cfbc9fc073609e209f48f996b34904af701
ayush.santri <ayushsantri222@gmail.com>
2023-04-04 21:46:54 +0530

    added new view controller

---
+++ Todo App MVVM/View/ViewControllers/add data/AddDataController.swift
@@ -0,0 +1,29 @@
+//
+//  AddDataController.swift
+//  Todo App MVVM
+//
+//  Created by Ayush Santri on 04/04/23.
+//
+
+import UIKit
+
+class AddDataController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
