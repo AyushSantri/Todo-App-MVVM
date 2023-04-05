8bb2b894a0aca32768c68a098c120bca43ff4724
ayush.santri <ayushsantri222@gmail.com>
2023-04-05 23:33:39 +0530

    configured stack view

--- Todo App MVVM/View/ViewControllers/add data/AddDataController.swift
+++ Todo App MVVM/View/ViewControllers/add data/AddDataController.swift
@@ -12,5 +12,27 @@ class AddDataController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Add Task"
+        configureStackView()
+    }
+    
+    let customTextField = CustomTextField()
+    let stackView = UIStackView()
+    
+    func configureStackView() {
+        view.addSubview(stackView)
+        
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+    }
+    
+    func setStackViewConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
     }
 }
