74d1b6e91c425b27a2e6263a139a3f6e4e236274
ayush.santri <ayushsantri222@gmail.com>
2023-04-08 21:50:55 +0530

    created custom label

--- Todo App MVVM/View/CustomViews/customLabel.swift
+++ Todo App MVVM/View/CustomViews/customLabel.swift
@@ -8,5 +8,16 @@
 import UIKit
 
 class customLabel: UILabel {
-
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpLabel() {
+        textColor = .darkText
+        textAlignment = .center
+    }
 }
