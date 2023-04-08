b1e8acd3ecbe91165f327c6d62083f6e44fd00d5
ayush.santri <ayushsantri222@gmail.com>
2023-04-08 21:57:00 +0530

    configured show data controller

--- Todo App MVVM/View/ViewControllers/showDetailedTODO/showDetailedTODOViewController.swift
+++ Todo App MVVM/View/ViewControllers/showDetailedTODO/showDetailedTODOViewController.swift
@@ -8,8 +8,27 @@
 import UIKit
 
 class showDetailedTODOViewController: UIViewController {
+    let titleLabel = customLabel()
+    let descriptionLabel = customLabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpLabel()
+    }
+    
+    
+    func setUpLabel() {
+        view.addSubview(titleLabel)
+        view.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: 20),
+            
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+        ])
     }
 }
