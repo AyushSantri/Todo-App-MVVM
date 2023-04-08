3b6e0802a79ec40ccf5c6096e910163d065a37a8
ayush.santri <ayushsantri222@gmail.com>
2023-04-08 21:40:00 +0530

    added a new view controller to show detailed task

---
+++ Todo App MVVM/View/ViewControllers/showDetailedTODO/showDetailedTODOViewController.swift
@@ -0,0 +1,15 @@
+//
+//  showDetailedTODOViewController.swift
+//  Todo App MVVM
+//
+//  Created by Ayush Santri on 08/04/23.
+//
+
+import UIKit
+
+class showDetailedTODOViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
